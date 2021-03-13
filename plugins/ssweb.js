88f37944fb08d49385ecce0ffa3a0390d439b0ce
KIssShot-Shinobu <71532094+KIssShot-Shinobu@users.noreply.github.com>
2021-03-13 17:57:13 +0700

    Update ssweb.js

--- plugins/ssweb.js
+++ plugins/ssweb.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, command, args }) => {
 handler.help = ['ss', 'ssf'].map(v => v + ' <url>')
 handler.tags = ['internet']
 handler.command = /^ss(web)?f?$/i
-handler.owner = false
+handler.owner = true
 handler.mods = false
 handler.premium = false
 handler.group = false
