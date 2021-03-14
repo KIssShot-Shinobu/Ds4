15fd3cb340f99d3d19cadc25e0cdb029d571ebcd
KIssShot-Shinobu <71532094+KIssShot-Shinobu@users.noreply.github.com>
2021-03-14 08:10:20 +0700

    Update meta.js

--- plugins/meta.js
+++ plugins/meta.js
@@ -1,7 +1,5 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, command, args }) => {
-  
-  if (!args[0]) return conn.reply(m.chat, 'Tidak ada url', m)
   let url = 'https://www.duellinksmeta.com/tier-list/'
   let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).buffer()
   conn.sendFile(m.chat, ss, 'screenshot.png', url, m)
