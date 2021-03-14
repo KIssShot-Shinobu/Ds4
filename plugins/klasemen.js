4a812bb2a00780abb7d831c2204b52af6052c66b
KIssShot-Shinobu <71532094+KIssShot-Shinobu@users.noreply.github.com>
2021-03-14 17:30:58 +0700

    Update klasemen.js

--- plugins/klasemen.js
+++ plugins/klasemen.js
@@ -1,7 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, command, args }) => {
   let full = 'https://www.premierleague.com/tables'
-  if (!args[0]) return conn.reply(m.chat, 'Tidak ada url', m)
   let url = 'https://www.premierleague.com/tables'
   let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).buffer()
   conn.sendFile(m.chat, ss, 'screenshot.png', url, m)
